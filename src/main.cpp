5a86c3865854e5f5c578c08c7a81827247336be9
Felicity1229 <123090312@link.cuhk.edu.cn>
2025-11-23 21:46:33 +0800

    Add comments for alternative UI testing in main.cpp
    Added comments for testing UI with Qt framework.

--- src/main.cpp
+++ src/main.cpp
@@ -7,3 +7,18 @@ int main() {
     game.startGame();
     return 0;
 }
+
+
+
+// run this main.cpp instead for testing ui
+// #include "mainwindow.h"
+
+// #include <QApplication>
+
+// int main(int argc, char *argv[])
+// {
+//     QApplication a(argc, argv);
+//     MainWindow w;
+//     w.show();
+//     return a.exec();
+// }
