69fd6b8d201923896a58bd56db2638fc105c2074
ceeceeli <1922464511@qq.com>
2025-12-12 18:35:09 +0800

    Remove build folder from repository

--- build/Windows-Release/.qt/QtDeployTargets-Debug.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_CUHKSZ_Monopoly_Online_FILE D:/CICILI/CSC3002/CUHKSZ-Monoploy-online/build/Windows-Release/Debug/CUHKSZ_Monopoly_Online.exe)
-set(__QT_DEPLOY_TARGET_CUHKSZ_Monopoly_Online_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_CUHKSZ_Monopoly_Online_RUNTIME_DLLS C:/Qt/6.10.1/msvc2022_64/bin/Qt6Widgetsd.dll;C:/Qt/6.10.1/msvc2022_64/bin/Qt6WebSocketsd.dll;C:/Qt/6.10.1/msvc2022_64/bin/Qt6Guid.dll;C:/Qt/6.10.1/msvc2022_64/bin/Qt6Networkd.dll;C:/Qt/6.10.1/msvc2022_64/bin/Qt6Cored.dll)
--- build/Windows-Release/.qt/QtDeployTargets-Release.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_CUHKSZ_Monopoly_Online_FILE D:/CICILI/CSC3002/CUHKSZ-Monoploy-online/build/Windows-Release/Release/CUHKSZ_Monopoly_Online.exe)
-set(__QT_DEPLOY_TARGET_CUHKSZ_Monopoly_Online_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_CUHKSZ_Monopoly_Online_RUNTIME_DLLS C:/Qt/6.10.1/msvc2022_64/bin/Qt6Widgets.dll;C:/Qt/6.10.1/msvc2022_64/bin/Qt6WebSockets.dll;C:/Qt/6.10.1/msvc2022_64/bin/Qt6Gui.dll;C:/Qt/6.10.1/msvc2022_64/bin/Qt6Network.dll;C:/Qt/6.10.1/msvc2022_64/bin/Qt6Core.dll)
